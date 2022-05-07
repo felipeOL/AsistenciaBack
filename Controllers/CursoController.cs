057aa8a67462f247dfac7d191d9bd83d45caa9a0
Vicente Calderón <thunderwearsvicho37@protonmail.com>
2022-05-07 17:28:12 -0400

    fix(Added bearer to the HTTP POST Curso Controller)

--- Controllers/CursoController.cs
+++ Controllers/CursoController.cs
@@ -1,10 +1,11 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authorization;
+
 namespace AsistenciaBack.Controller;
-[ApiController,Route("api/cursos"),EnableCors("FrontendCors")]//esto es para mapear el controlador
+[ApiController, Route("api/curso")]//esto es para mapear el controlador
 public class CursoController: ControllerBase
 {
     private readonly AppDbContext _context;
@@ -12,12 +13,11 @@ public class CursoController: ControllerBase
     {
         this._context = context;
     }
-    [Authorize(Roles ="Administrator"),HttpPost("Crear")]// en esta parte autorizamos el metodo y el usuario que lo puede usar.
-    public async Task<ActionResult>CrearCurso([FromBody] Curso curso)
+    [Authorize(AuthenticationSchemes = "Bearer", Roles = "Administrator"), HttpPost, EnableCors("FrontendCors")]// en esta parte autorizamos el metodo y el usuario que lo puede usar.
+    public async Task<ActionResult> CrearCurso([FromBody] Curso curso)
     {
         this._context.Cursos.Add(curso);// aqui lo agregamos al contexto, falta agregarlo a la DB
-        await this._context.SaveChangesAsync();// se aguada ne la DB de manera asincrona 
+        await this._context.SaveChangesAsync();// se aguada ne la DB de manera asincrona
         return this.Ok("se guardo con exito");// retornamos un mensaje
-
     }
 }
