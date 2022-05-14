adb5608b1c9be3e647365036c9c6b1c5db064e7e
Vicente Calderón <thunderwearsvicho37@protonmail.com>
2022-05-14 16:47:57 -0400

    feat(CourseController) List user courses depending on role

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -47,7 +47,7 @@ public class CourseController : ControllerBase
 		await this._context.SaveChangesAsync();
 		return this.Ok("(DEV) Curso guardado con éxito");
 	}
-	[Authorize(AuthenticationSchemes = "Bearer", Roles = "Administrator,Teacher"), HttpGet("todos"), Produces("application/json"), ProducesResponseType(StatusCodes.Status200OK), ProducesResponseType(StatusCodes.Status400BadRequest), ProducesResponseType(StatusCodes.Status500InternalServerError)]
+	[Authorize(AuthenticationSchemes = "Bearer", Roles = "Administrator,Teacher,Student"), HttpGet("todos"), Produces("application/json"), ProducesResponseType(StatusCodes.Status200OK), ProducesResponseType(StatusCodes.Status400BadRequest), ProducesResponseType(StatusCodes.Status500InternalServerError)]
 	public async Task<ActionResult<IEnumerable<CourseResponse>>> GetAllCourses()
 	{
 		if (this._context.Courses is null)
@@ -64,38 +64,32 @@ public class CourseController : ControllerBase
 			return this.StatusCode(StatusCodes.Status500InternalServerError, "(DEV) Usuario actual no encontrado");
 		}
 		var currentRoles = await this._userManager.GetRolesAsync(currentUser);
+		List<Course> courses;
 		if (currentRoles.Contains("Administrator"))
 		{
-			var courses = this._context.Courses.Include(c => c.Users).ToList();
-			var courseResponses = new List<CourseResponse>();
-			foreach (var course in courses)
-			{
-				var courseResponse = this._mapper.Map<CourseResponse>(course);
-				foreach (var user in course.Users)
-				{
-					var roles = await this._userManager.GetRolesAsync(user);
-					if (roles.Contains("Teacher"))
-					{
-						courseResponse.Professor = this._mapper.Map<UserResponse>(user);
-						courseResponse.Professor.Role = "Teacher";
-						break;
-					}
-				}
-				courseResponses.Add(courseResponse);
-			}
-			return courseResponses;
+			courses = this._context.Courses.Include(c => c.Users).ToList();
 		}
 		else
 		{
-			var query = this._context.Users.Where(u => u.Name == currentUser.Name).Include(u => u.Courses).FirstOrDefault();
-			if (query is null)
-			{
-				return this.StatusCode(StatusCodes.Status500InternalServerError, "(DEV) La query ha arrojado resultado nulo");
-			}
-			return query.Courses.Select(
-				c => this._mapper.Map<CourseResponse>(c)
-			).ToList();
+			courses = this._context.Courses.Include(c => c.Users).Where(c => c.Users.Contains(currentUser)).ToList();
 		}
+		var courseResponses = new List<CourseResponse>();
+		foreach (var course in courses)
+		{
+			var courseResponse = this._mapper.Map<CourseResponse>(course);
+			foreach (var user in course.Users)
+			{
+				var roles = await this._userManager.GetRolesAsync(user);
+				if (roles.Contains("Teacher"))
+				{
+					courseResponse.Professor = this._mapper.Map<UserResponse>(user);
+					courseResponse.Professor.Role = "Teacher";
+					break;
+				}
+			}
+			courseResponses.Add(courseResponse);
+		}
+		return courseResponses;
 	}
 	[Authorize(AuthenticationSchemes = "Bearer", Roles = "Teacher"), HttpPost("agregarEstudiante"), Produces("application/json"), ProducesResponseType(StatusCodes.Status200OK), ProducesResponseType(StatusCodes.Status400BadRequest), ProducesResponseType(StatusCodes.Status500InternalServerError)]
 	public async Task<ActionResult> AddStudentToCourse([FromBody] AddStudentToCourse request)
