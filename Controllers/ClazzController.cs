9d39eaae462d682eb2cf8c3f157fe8fa2ff7af2b
Vicente Calderón <thunderwearsvicho37@protonmail.com>
2022-07-01 22:08:46 -0400

    FromBody

--- Controllers/ClazzController.cs
+++ Controllers/ClazzController.cs
@@ -52,7 +52,7 @@ public class ClazzController : ControllerBase
 		return this.Ok($"Clase con fecha {request.Date} guardada con éxito en el curso {request.CourseId}");
 	}
 	[Authorize(AuthenticationSchemes = "Bearer", Roles = "Student,Teacher"), HttpPost("todosDesdeFecha"), Produces("application/json"), ProducesResponseType(StatusCodes.Status200OK), ProducesResponseType(StatusCodes.Status400BadRequest), ProducesResponseType(StatusCodes.Status500InternalServerError)]
-	public async Task<ActionResult<IEnumerable<ClazzResponse>>> GetAllClazzsFromDate([FromQuery] GetStudentClassesFromDate request)
+	public async Task<ActionResult<IEnumerable<ClazzResponse>>> GetAllClazzsFromDate([FromBody] GetStudentClassesFromDate request)
 	{
 		var currentUser = await this._userManager.FindByIdAsync(this.HttpContext.User.Identity.Name);
 		var courses = this._context.Courses.Include(c => c.Users).Where(c => c.Users.Contains(currentUser)).ToList();
